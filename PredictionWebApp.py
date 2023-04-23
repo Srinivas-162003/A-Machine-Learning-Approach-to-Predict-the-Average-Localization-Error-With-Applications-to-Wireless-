5e3fca63b259381367e6c2e40da9237b7b5df0cf
Srinivas-162003 <100286844+Srinivas-162003@users.noreply.github.com>
2023-04-23 23:25:53 +0530

    Update PredictionWebApp.py

--- PredictionWebApp.py
+++ PredictionWebApp.py
@@ -29,7 +29,7 @@ def get_img_as_base64(file):
     return base64.b64encode(data).decode()
 
 
-img = get_img_as_base64("D:\Deployment\Background.jpg")
+#img = get_img_as_base64("D:\Deployment\Background.jpg")
 
 page_bg_img = f"""
 <style>
