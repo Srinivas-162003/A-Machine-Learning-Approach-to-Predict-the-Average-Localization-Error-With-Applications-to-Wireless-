91f6c292d631c9a0077a474e698575f5126f1dd6
Srinivas-162003 <100286844+Srinivas-162003@users.noreply.github.com>
2023-04-23 20:36:34 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import sklearn
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
