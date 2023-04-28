3e4020481f17a9c424567249ef554e462c6dc1e1
Srinivas-162003 <100286844+Srinivas-162003@users.noreply.github.com>
2023-04-28 21:16:45 +0530

    Update PredictionWebApp.py

--- PredictionWebApp.py
+++ PredictionWebApp.py
@@ -47,6 +47,8 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 
 def main():
     st.markdown('<style>h1, h2, h3, h4, h5, h6, p {color: white !important;}</style>', unsafe_allow_html=True)
+    st.markdown('<style>div.row-widget.stButton>button:first-child {background-color: red; color: white;}</style>', unsafe_allow_html=True)
+
     st.title("Prediction of error in wireless sensor networks Web app")
 
     anchor_ratio = st.text_input("Enter the anchor_ratio: ")
